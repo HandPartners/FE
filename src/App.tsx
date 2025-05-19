1d68730d91210676f9c85640414ab7a0e7cafb7d
Junhyeok <cjh64866@naver.com>
2025-05-19 13:10:48 +0900

    feat: NavBar 추가, text.css 추가, clsx 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import DefaultLayout from "./layouts/DefaultLayout";
+import About from "./pages/About";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <DefaultLayout />,
     children: [{ index: true, element: <p className="text-red-500">Hello</p> }],
+  },
+  {
+    path: "/about",
+    element: <DefaultLayout />,
+    children: [{ index: true, element: <About /> }],
   },
 ]);
 
