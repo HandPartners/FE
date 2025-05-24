31829a3eb30ce63a0c43af833006c3ae27ca6ae2
goodsmell <87801306+goodsmell@users.noreply.github.com>
2025-05-24 20:00:45 +0900

    feat: main 페이지 스크롤 다운 시 페이드인 효과 (#18)
    * feat: fade in hook
    
    * feat: main 페이지 스크롤 다운 시 페이드인 효과

---
+++ src/components/main/FadeInItem.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import useFadeIn from "../../hook/useFadeIn";
+interface FadeInItemProps {
+  children: React.ReactNode;
+}
+
+const FadeInItem: React.FC<FadeInItemProps> = ({ children }) => {
+  const { ref, isVisible } = useFadeIn();
+
+  return (
+    <div
+      ref={ref}
+      className={`transition-opacity duration-1000 ease-in-out ${
+        isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-4"
+      }`}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default FadeInItem;
