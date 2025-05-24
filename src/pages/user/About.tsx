71e9afa8df7ff84ef1063d8fd2d6b909b111cd65
goodsmell <87801306+goodsmell@users.noreply.github.com>
2025-05-24 20:40:44 +0900

    feat: about 페이지 fade in 적용

--- src/pages/user/About.tsx
+++ src/pages/user/About.tsx
@@ -2,10 +2,11 @@ import EachTeam from "../../components/about/EachTeam";
 import BGTop from "../../components/BGTop";
 import TitleLine from "../../components/TitleLine";
 import bannerImg from "../../assets/images/banner/aboutBanner.png";
+import FadeInItem from "../../components/main/FadeInItem";
 
 const About = () => {
   return (
-    <main className=" flex flex-col items-center w-[1280px] h-full mx-auto relative">
+    <main className=" flex flex-col items-center w-[1280px] mx-auto relative pb-[947px]">
       <div className="absolute top-[99px] left-[85px] z-10 flex flex-col  gap-[36px] w-fit">
         <h1 className="text-[48px] font-bold leading-[60px] tracking-[-0.96px]">
           About
@@ -14,33 +15,38 @@ const About = () => {
       </div>
       <BGTop testBenner={bannerImg} />
 
-      <section className="flex flex-col gap-[371px]  mt-[100px] mx-auto w-full h-full">
+      <section className="flex flex-col gap-[371px]  mt-[100px] mx-auto w-full h-full ">
         <section className="flex flex-col gap-[100px] w-full">
-          <section className="flex flex-col gap-[50px] w-full">
-            <TitleLine>WHAT WE DO</TitleLine>
-            <article className="flex flex-col gap-[16px]">
-              <h3 className="h3-bold">우리는 스타트업의 성장을 돕습니다.</h3>
-              <h5 className="h5-medium">
-                우리는 스타트업의 성장을 돕습니다. 이렇게 돕습니다. 우리는
-                스타트업의 성장을 돕습니다. 이렇게 돕습니다. <br />
-                우리는 스타트업의 성장을 돕습니다. 이렇게 돕습니다. 우리는
-                스타트업의 성장을 돕습니다. 이렇게 돕습니다. <br />
-                우리는 스타트업의 성장을 돕습니다. 이렇게 돕습니다. 우리는
-                스타트업의 성장을 돕습니다. 이렇게 돕습니다.
-                <br />
-                우리는 스타트업의 성장을 돕습니다. 이렇게 돕습니다.{" "}
-              </h5>
-            </article>
-          </section>
+          <FadeInItem>
+            <section className="flex flex-col gap-[50px] w-full">
+              <TitleLine>WHAT WE DO</TitleLine>
 
-          <section className="flex flex-col gap-[50px] w-full">
-            <TitleLine>TEAM</TitleLine>
-            <article className="grid grid-cols-5 gap-y-[100px] justify-items-center w-full">
-              {Array.from({ length: 10 }).map((_, index) => (
-                <EachTeam key={index} />
-              ))}
-            </article>
-          </section>
+              <article className="flex flex-col gap-[16px]">
+                <h3 className="h3-bold">우리는 스타트업의 성장을 돕습니다.</h3>
+                <h5 className="h5-medium">
+                  우리는 스타트업의 성장을 돕습니다. 이렇게 돕습니다. 우리는
+                  스타트업의 성장을 돕습니다. 이렇게 돕습니다. <br />
+                  우리는 스타트업의 성장을 돕습니다. 이렇게 돕습니다. 우리는
+                  스타트업의 성장을 돕습니다. 이렇게 돕습니다. <br />
+                  우리는 스타트업의 성장을 돕습니다. 이렇게 돕습니다. 우리는
+                  스타트업의 성장을 돕습니다. 이렇게 돕습니다.
+                  <br />
+                  우리는 스타트업의 성장을 돕습니다. 이렇게 돕습니다.{" "}
+                </h5>
+              </article>
+            </section>
+          </FadeInItem>
+
+          <FadeInItem>
+            <section className="flex flex-col gap-[50px] w-full">
+              <TitleLine>TEAM</TitleLine>{" "}
+              <article className="grid grid-cols-5 gap-y-[100px] justify-items-center w-full">
+                {Array.from({ length: 10 }).map((_, index) => (
+                  <EachTeam key={index} />
+                ))}
+              </article>
+            </section>
+          </FadeInItem>
         </section>
       </section>
     </main>
