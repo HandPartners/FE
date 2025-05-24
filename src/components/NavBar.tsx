0a8c769d7dc100ee7a1e5c11189f05e65b5f6a08
goodsmell <87801306+goodsmell@users.noreply.github.com>
2025-05-24 16:07:19 +0900

    feat: nav bar 수정 (Ver. 상세설계 05.22)

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,52 +1,70 @@
 import { NavLink } from "react-router-dom";
 import clsx from "clsx";
+import logo from "../assets/logo.svg";
 
 const NavBar = () => {
-  const defaultClassName = "h5-bold cursor-pointer";
   return (
-    <nav className="flex justify-center items-center relative h-[70px] z-10">
-      <div className="flex justify-between items-center w-[calc(100%-640px)]">
-        <div>logo</div>
+    <nav
+      className="flex justify-center items-center relative h-[70px] w-screen z-10 "
+      style={{ boxShadow: "0px 2px 3.5px 0px var(--Main, #2E3092)" }}
+    >
+      <div className="flex justify-between items-center w-[64%] min-w-[1228px] ">
+        <img src={logo} alt="회사 로고"></img>
         <div className="flex gap-[64px]">
-          <NavLink
-            to="/about"
-            className={({ isActive }) => {
-              return clsx(isActive && "text-main", defaultClassName);
-            }}
-          >
-            About
+          <NavLink to="/about">
+            {({ isActive }) => (
+              <span
+                className={` cursor-pointer inline-block w-[57px] text-center ${clsx(
+                  isActive ? "h5-bold text-main" : "h5-medium"
+                )}`}
+              >
+                About
+              </span>
+            )}
           </NavLink>
-          <NavLink
-            to="/program"
-            className={({ isActive }) => {
-              return clsx(isActive && "text-main", defaultClassName);
-            }}
-          >
-            Program
+          <NavLink to="/program">
+            {({ isActive }) => (
+              <span
+                className={` cursor-pointer inline-block w-[75px] text-center ${clsx(
+                  isActive ? "h5-bold text-main" : "h5-medium"
+                )}`}
+              >
+                Program
+              </span>
+            )}
           </NavLink>
-          <NavLink
-            to="/portfolio"
-            className={({ isActive }) => {
-              return clsx(isActive && "text-main", defaultClassName);
-            }}
-          >
-            Portfolio
+          <NavLink to="/portfolio">
+            {({ isActive }) => (
+              <span
+                className={` cursor-pointer inline-block w-[74px] text-center ${clsx(
+                  isActive ? "h5-bold text-main" : "h5-medium"
+                )}`}
+              >
+                Portfolio
+              </span>
+            )}
           </NavLink>
-          <NavLink
-            to="/news"
-            className={({ isActive }) => {
-              return clsx(isActive && "text-main", defaultClassName);
-            }}
-          >
-            News
+          <NavLink to="/news">
+            {({ isActive }) => (
+              <span
+                className={` cursor-pointer inline-block w-[51px] text-center ${clsx(
+                  isActive ? "h5-bold text-main" : "h5-medium"
+                )}`}
+              >
+                News
+              </span>
+            )}
           </NavLink>
-          <NavLink
-            to="/contact"
-            className={({ isActive }) => {
-              return clsx(isActive && "text-main", defaultClassName);
-            }}
-          >
-            Contact
+          <NavLink to="/contact">
+            {({ isActive }) => (
+              <span
+                className={` cursor-pointer inline-block w-[71px] text-center ${clsx(
+                  isActive ? "h5-bold text-main" : "h5-medium"
+                )}`}
+              >
+                Contact
+              </span>
+            )}
           </NavLink>
         </div>
       </div>
