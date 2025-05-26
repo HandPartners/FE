183cc6c740b3745540b668c36c958bad6dd90c57
goodsmell <87801306+goodsmell@users.noreply.github.com>
2025-05-26 22:15:53 +0900

    fix: arrow이미지 경로 수정

--- src/components/portfolio/modal/select/DropdownIndicator.tsx
+++ src/components/portfolio/modal/select/DropdownIndicator.tsx
@@ -1,7 +1,7 @@
 import { components } from "react-select";
 import type { DropdownIndicatorProps } from "react-select";
-import selectDownArrow from "../../assets/icons/ic_down_arrow.svg";
-import selectUpArrow from "../../assets/icons/ic_up_arrow.svg";
+import selectDownArrow from "../../../../assets/icons/ic_down_arrow.svg";
+import selectUpArrow from "../../../../assets/icons/ic_up_arrow.svg";
 import type { Category } from "../../../../types/category";
 
 const DropdownIndicator = (props: DropdownIndicatorProps<Category, false>) => {
