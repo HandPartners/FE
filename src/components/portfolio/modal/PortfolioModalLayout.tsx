767b03047571658b626fab4c5149f43c5acda479
goodsmell <87801306+goodsmell@users.noreply.github.com>
2025-05-26 21:04:57 +0900

    fix: 모달 뒷배경 스크롤 금지

--- src/components/portfolio/modal/PortfolioModalLayout.tsx
+++ src/components/portfolio/modal/PortfolioModalLayout.tsx
@@ -1,4 +1,6 @@
 // 모달 전체 레이아웃
+import { useEffect } from "react";
+
 interface PortfolioModalProps {
   children: React.ReactNode;
   marginTop: string;
@@ -8,8 +10,16 @@ const PortfolioModalLayout: React.FC<PortfolioModalProps> = ({
   children,
   marginTop,
 }) => {
+  // 모달 열릴 때 body 스크롤 방지
+  useEffect(() => {
+    const originalStyle = window.getComputedStyle(document.body).overflow;
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = originalStyle; // cleanup
+    };
+  }, []);
   return (
-    <div className="fixed inset-0 flex justify-center  z-50 bg-[rgba(217,217,217,0.3)] backdrop-blur-sm">
+    <div className="fixed inset-0 flex justify-center  overflow-y-auto z-50 bg-[rgba(217,217,217,0.3)] backdrop-blur-sm">
       <div
         className={`${marginTop} bg-white py-[36px] px-[30px] rounded-lg w-[500px] h-fit relative flex flex-col gap-[28px]`}
       >
