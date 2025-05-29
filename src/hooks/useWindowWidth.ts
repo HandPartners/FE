9cd3cce3b779d56d162b9e9ac32ba2c1f0969b5b
Junhyeok <cjh64866@naver.com>
2025-05-29 18:44:29 +0900

    feat: useWindowWidth hook 추가

---
+++ src/hooks/useWindowWidth.ts
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+const useWindowWidth = () => {
+  const [windowWidth, setWindowWidth] = useState({
+    sm: window.innerWidth >= 640,
+    md: window.innerWidth >= 768,
+    lg: window.innerWidth >= 1024,
+    xl: window.innerWidth >= 1280,
+    xxl: window.innerWidth >= 1536,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth({
+        sm: window.innerWidth >= 640,
+        md: window.innerWidth >= 768,
+        lg: window.innerWidth >= 1024,
+        xl: window.innerWidth >= 1280,
+        xxl: window.innerWidth >= 1536,
+      });
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return windowWidth;
+};
+
+export default useWindowWidth;
