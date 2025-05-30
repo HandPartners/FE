e305981c27620fd91064c252efc2d9cccd4aa619
goodsmell <87801306+goodsmell@users.noreply.github.com>
2025-05-31 00:51:41 +0900

    feat: program 반응형 적용

--- src/components/program/ProgramSection.tsx
+++ src/components/program/ProgramSection.tsx
@@ -1,5 +1,6 @@
 import TitleLine from "../../components/TitleLine";
 import FadeInItem from "../../components/main/FadeInItem";
+import useWindowWidth from "../../hooks/useWindowWidth";
 
 interface ProgramSectionProps {
   title: string;
@@ -15,19 +16,32 @@ const ProgramSection: React.FC<ProgramSectionProps> = ({
   imgUrl,
   onClick,
 }) => {
+  const windowWidth = useWindowWidth();
+  const isMobile = !windowWidth.md;
+
   return (
     <FadeInItem>
-      <section className="flex flex-col gap-[50px] w-full">
+      <section className="flex flex-col gap-[20px] md:gap-[50px] w-full">
         <TitleLine>{title}</TitleLine>
-        <article className="flex flex-row gap-[4.6%] h-[360px]">
-          <img src={imgUrl} alt="이미지" className="w-[42%] h-full" />
-          <div className="flex flex-col justify-between py-[25px] ">
-            <div className="flex flex-col gap-[24px]">
-              <h3 className="h2-bold">{subTitle}</h3>
-              <h5 className="h5-medium ">{contents}</h5>
+        <article className="flex flex-col md:flex-row gap-[4.6%] md:h-[360px] px-[15px]  md:px-0">
+          <img
+            src={imgUrl}
+            alt="이미지"
+            className=" w-full aspect-[3/2] object-cover  md:w-[42%] md:h-full "
+          />
+          <div className="flex flex-col md:justify-between py-[25px] gap-[30px] md:gap-0">
+            <div className="flex flex-col gap-[10px] md:gap-[24px]">
+              <h3 className={`${isMobile ? "h5-bold" : "h2-bold"}`}>
+                {subTitle}
+              </h3>
+              <h5 className={`${isMobile ? "p-medium-medium" : "h5-medium "}`}>
+                {contents}
+              </h5>
             </div>
             <span
-              className="cursor-pointer h4-bold text-[var(--sub)] transition-colors duration-250 ease-in-out hover:text-[#8cd2ee]"
+              className={`cursor-pointer  text-[var(--sub)] transition-colors duration-250 ease-in-out hover:text-[#8cd2ee] ${
+                isMobile ? "p-medium-bold" : "h4-bold "
+              }`}
               onClick={onClick}
             >
               진행 사례 보기 →
--- src/pages/user/Program.tsx
+++ src/pages/user/Program.tsx
@@ -4,26 +4,33 @@ import ProgramSection from "../../components/program/ProgramSection";
 import { useNavigate } from "react-router-dom";
 import bannerImg from "../../assets/images/banner/ProgramBanner.png";
 import ScrollToTopButton from "../../components/ScrollToTopButton";
+import useWindowWidth from "../../hooks/useWindowWidth";
 const Program = () => {
   const navigate = useNavigate();
 
   const isAdmin = window.location.pathname.includes("admin");
+  const windowWidth = useWindowWidth();
+  const isMobile = !windowWidth.md;
 
   return (
-    <main className=" flex flex-col items-center w-[1280px] max-w-full h-full mx-auto relative">
-      <div className="absolute top-[99px] left-[85px] z-10 flex flex-col  gap-[36px] w-fit">
-        <h1 className="text-[48px] font-bold leading-[60px] tracking-[-0.96px]">
-          Program
-        </h1>
-        <h3 className="h3-medium">한줄 소개</h3>
+    <main className=" flex flex-col items-center w-[1280px] max-w-full h-full mx-auto relative ">
+      <div className="absolute top-[34px] left-[60px] md:top-[99px] md:left-[85px] z-10 flex flex-col gap-[3px] md:gap-[36px] w-fit">
+        <h1 className={` ${isMobile ? "h4-bold" : "md-banner"}`}>Program</h1>
+        <h3 className={` ${isMobile ? "p-small-medium" : "h3-medium"}`}>
+          한줄 소개
+        </h3>
       </div>
       <BGTop testBenner={bannerImg} />
-      <section className="flex flex-col gap-[371px]  h-full mt-[100px] ">
-        <section className="flex flex-col gap-[200px] mx-auto w-[1280px] pb-[346px] ">
+      <section className="flex flex-col gap-[371px]  h-full mt-[50px] md:mt-[100px] ">
+        <section className="flex flex-col gap-[100px] md:gap-[200px] mx-auto w-[84.7svw] md:w-[1280px] pb-[346px] ">
           <ProgramSection
             title={"Phase 1. Consulting"}
             subTitle="창업자들을 대상으로 컨설팅 진행"
-            contents="이런수업을 진행합니다.이런수업을 진행합니다.이런수업을 진행합니다.이런수업을 진행합니다.이런수업을 진행합니다.이런수업을 진행합니다.이런수업을 진행합니다.이런수업을 진행합니다.이런수업을 진행합니다.이런수업을 진행합니다.이런수업을 진행합니다.이런수업을 진행합니다.이런수업을 진행합니다.이런수업을 진행합니다.이런수업을 진행합니다."
+            contents="이런 수업을 진행합니다.이런 수업을 진행합니다.
+이런 수업을 진행합니다.이런 수업을 진행합니다.
+이런 수업을 진행합니다.이런 수업을 진행합니다.
+이런 수업을 진행합니다.이런 수업을 진행합니다.
+이런 수업을 진행합니다.이런 수업을 진행합니다."
             imgUrl={testImg}
             onClick={() => {
               sessionStorage.setItem("newsTab", "Consulting");
