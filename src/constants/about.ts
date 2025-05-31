0c6b7462988b36bcc279304d13cdb5a15dc87023
Junhyeok <cjh64866@naver.com>
2025-05-31 15:31:47 +0900

    feat: About crew 추가용 about.ts 생성

---
+++ src/constants/about.ts
@@ -0,0 +1,23 @@
+import 더미 from "./../assets/images/about/Rectangle 830384.png";
+
+interface About {
+  name: string;
+  position: string;
+  photo: string;
+}
+
+/* 복사하여 사용
+    {
+      name: "",
+      position: "",
+      photo: ,
+    },
+  */
+
+export const about: About[] = [
+  {
+    name: "더미",
+    position: "더미",
+    photo: 더미,
+  },
+];
