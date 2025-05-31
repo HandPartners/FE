e2612617a98a349f4a51ae2d9554a5d59e1daac7
Junhyeok <cjh64866@naver.com>
2025-05-31 15:33:08 +0900

    feat: About 반응형 및 about.ts 연결

--- src/components/about/EachTeam.tsx
+++ src/components/about/EachTeam.tsx
@@ -1,21 +1,31 @@
 import clsx from "clsx";
 import useWindowWidth from "../../hooks/useWindowWidth";
+import type React from "react";
 
-const EachTeam = () => {
+interface EachTeamProps {
+  name: string;
+  position: string;
+  photo: string;
+}
+
+const EachTeam: React.FC<EachTeamProps> = ({ name, position, photo }) => {
   const { md } = useWindowWidth();
   return (
     <div className="flex flex-col items-center w-full">
-      <div className="w-full max-w-[152.489px] md:max-w-[219px] aspect-[219/270] bg-[#D9D9D9]"></div>
+      <div
+        className="w-full max-w-[152.489px] md:max-w-[219px] aspect-[219/270] bg-[#D9D9D9] bg-cover bg-no-repeat bg-center"
+        style={{ backgroundImage: `url(${photo})` }}
+      ></div>
       <h4
         className={clsx(
           "mt-[18px] md:mt-[20px] mb-[6px] md:mb-[15px] text-center",
           md ? "h4-bold" : "p-large-bold"
         )}
       >
-        이름
+        {name}
       </h4>
       <h5 className={clsx("text-center", md ? "h5-medium" : "p-medium-medium")}>
-        직책
+        {position}
       </h5>
     </div>
   );
