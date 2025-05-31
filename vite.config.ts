4ccd7cfba02b3688971f5d1c79cfc0d62a2b1bb3
Choi JunHyeok <cjh64866@naver.com>
2025-05-31 13:49:46 +0900

    Revert "feat: webp parsing 추가"

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { imageToWebpPlugin } from "vite-plugin-image-to-webp";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), imageToWebpPlugin()],
-});
+  plugins: [react()],
+})
