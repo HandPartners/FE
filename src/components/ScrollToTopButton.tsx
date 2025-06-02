62a09f64854a4e565b984ba5e57c5e56da5cecda
goodsmell <87801306+goodsmell@users.noreply.github.com>
2025-06-02 17:11:46 +0900

    fix: 상단 이동 플로팅 버튼 위치 수정

--- src/components/ScrollToTopButton.tsx
+++ src/components/ScrollToTopButton.tsx
@@ -2,7 +2,7 @@ const ScrollToTopButton = () => {
   return (
     <button
       onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
-      className="cursor-pointer fixed bottom-[50px] right-[50px] z-50 w-[40px] h-[40px] rounded-full hover:bg-[#f5f5f5]  bg-[var(--white)] text-[20px] shadow-lg flex items-center justify-center hover:bg-opacity-80 transition-colors"
+      className="cursor-pointer fixed bottom-[50px] right-[7.63svw] md:right-[3.9svw] z-50 w-[40px] h-[40px] rounded-full hover:bg-[#f5f5f5]  bg-[var(--white)] text-[20px] shadow-lg flex items-center justify-center hover:bg-opacity-80 transition-colors"
       style={{ boxShadow: "1px 1px 4px rgba(0, 0, 0, 0.25)" }}
     >
       ⌅
