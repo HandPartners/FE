478bcf77d4371c8088409902caabda14e3293972
goodsmell <87801306+goodsmell@users.noreply.github.com>
2025-06-02 17:29:34 +0900

    fix: 모바일 환경에서 스크롤 되는 문제

--- src/components/portfolio/modal/PortfolioModalLayout.tsx
+++ src/components/portfolio/modal/PortfolioModalLayout.tsx
@@ -12,18 +12,31 @@ const PortfolioModalLayout: React.FC<PortfolioModalProps> = ({
   marginTop,
   onClickBG,
 }) => {
-  // 모달 열릴 때 body 스크롤 방지
   useEffect(() => {
-    const originalStyle = window.getComputedStyle(document.body).overflow;
-    document.body.style.overflow = "hidden";
+    // 현재 스크롤 위치를 저장
+    const scrollY = window.scrollY;
+
+    // body를 고정해서 스크롤 방지
+    document.body.style.position = "fixed"; // body를 고정
+    document.body.style.top = `-${scrollY}px`; // 고정 위치를 현재 스크롤 위치로 설정
+    document.body.style.overflowY = "scroll";
+    document.body.style.width = "100%"; // 고정 시 일부 브라우저에서 가로 스크롤 생기는 문제 방지
+
+    //cleanup 함수 (모달 닫힐 때 실행)
     return () => {
-      document.body.style.overflow = originalStyle; // cleanup
+      document.body.style.position = "";
+      document.body.style.top = "";
+      document.body.style.overflowY = "";
+      document.body.style.width = "";
+
+      // 모달 열기 전 위치로 스크롤 복구
+      window.scrollTo(0, scrollY);
     };
   }, []);
 
   return (
     <div
-      className="fixed inset-0 flex justify-center  min-h-screen  overflow-y-auto z-50 bg-[rgba(217,217,217,0.3)] backdrop-blur-sm"
+      className="fixed inset-0 flex justify-center  min-h-screen  z-50 bg-[rgba(217,217,217,0.3)] backdrop-blur-sm"
       onClick={onClickBG}
     >
       <div
