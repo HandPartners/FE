ed5fa478c44666981a39d39533d571f622b07a9a
goodsmell <87801306+goodsmell@users.noreply.github.com>
2025-06-08 01:42:25 +0900

    fix: news edit 인풋 포커스 시 색상, 선 삭제

--- src/components/news/NewsEditLinkBtnInput.tsx
+++ src/components/news/NewsEditLinkBtnInput.tsx
@@ -13,7 +13,7 @@ const NewsEditLinkBtnInput: React.FC<NewsEditLinkBtnInputProps> = ({
     <div className="flex flex-col gap-[8px] w-[356px]">
       <p className="p-medium-medium text-[#777]">{children}</p>
       <input
-        className="py-[10px] pl-[20px] pr-[10px] bg-[#f7f9fa] rounded-[12px]"
+        className="py-[10px] pl-[20px] pr-[10px] bg-[#f7f9fa] rounded-[12px] focus:outline-none focus:ring-0 focus:bg-[#f7f9fa]"
         type="text"
         {...props}
       />
