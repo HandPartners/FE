14be0360dc99b980714cb75dbeefc95c544141a0
Junhyeok <cjh64866@naver.com>
2025-10-28 19:48:43 +0900

    rename: 사진 파일 이름 영어로 변경

--- src/assets/images/about/index.ts
+++ src/assets/images/about/index.ts
@@ -1,11 +1,8 @@
 export { default as dummy } from "./Rectangle 830384.png"
-export { default as 구일호 } from "./구일호.png";
-export { default as 김민정 } from "./김민정.png";
-export { default as 김상우 } from "./김상우.png";
-export { default as 박상아 } from "./박상아.png";
-export { default as 정일혁 } from "./정일혁.png";
-export { default as 최서윤 } from "./최서윤.png";
-export { default as 홍성진 } from "./홍성진.png";
-
-
-
+export { default as 구일호 } from "./KuIlHo.png";
+export { default as 김민정 } from "./KimMinJeong.png";
+export { default as 김상우 } from "./KimSangu.png";
+export { default as 박상아 } from "./ParkSangA.png";
+export { default as 정일혁 } from "./JeongIlHyeok.png";
+export { default as 최서윤 } from "./ChoiSeoYun.png";
+export { default as 홍성진 } from "./HongSeongJin.png";
