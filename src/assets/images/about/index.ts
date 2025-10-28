4722609db8bcf9c778a7c1b0392e1ccaea76187d
Junhyeok <cjh64866@naver.com>
2025-10-28 20:07:18 +0900

    rename: 오타 수정

--- src/assets/images/about/index.ts
+++ src/assets/images/about/index.ts
@@ -1,7 +1,7 @@
 export { default as dummy } from "./Rectangle 830384.png"
 export { default as 구일호 } from "./KuIlHo.png";
 export { default as 김민정 } from "./KimMinJeong.png";
-export { default as 김상우 } from "./KimSangu.png";
+export { default as 김상우 } from "./KimSangU.png";
 export { default as 박상아 } from "./ParkSangA.png";
 export { default as 정일혁 } from "./JeongIlHyeok.png";
 export { default as 최서윤 } from "./ChoiSeoYun.png";
